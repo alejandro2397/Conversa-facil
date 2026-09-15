e1e86411d1665554cc8e72d38544112a3e09aa4e
alejandro2397 <72469464+alejandro2397@users.noreply.github.com>
2026-09-15 16:42:46 -0600

    Add on-device translation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,5 +29,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.google.mlkit:translate:17.0.3")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
