28cfff8b21068347262f83121383d7d5114062c3
alejandro2397 <72469464+alejandro2397@users.noreply.github.com>
2026-09-20 16:40:07 -0600

    Update Android target SDK to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 
 android {
     namespace = "com.alejandro.conversafacil"
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.alejandro.conversafacil"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
