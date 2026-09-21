bc1fd42b88ef68a6a6b60f03aa35f2cc0c947ca5
alejandro2397 <72469464+alejandro2397@users.noreply.github.com>
2026-09-20 21:37:46 -0600

    Use available AdMob Lite SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,6 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.mlkit:translate:17.0.3")
-    implementation("com.google.android.gms:play-services-ads-lite:24.7.0")
+    implementation("com.google.android.gms:play-services-ads-lite:25.0.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
